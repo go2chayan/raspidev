e6f20e191b9809b1ef0808d95e97764bec156450
Md. Iftekhar Tanveer <go2chayan@gmail.com>
2022-03-20 23:42:41 -0400

    Update install_python.sh

--- install_python.sh
+++ install_python.sh
@@ -4,6 +4,6 @@ sudo apt-get update \
     && sudo wget -q http://repo.continuum.io/miniconda/Miniconda3-latest-Linux-armv7l.sh \
     && bash ./Miniconda3-latest-Linux-armv7l.sh -b -p ~/bin/conda \
     && ~/bin/conda/bin/conda config --add channels rpi \
-    && ~/bin/conda/bin/conda create -y -n raspi_venv python=3.7 pip \
+    && ~/bin/conda/bin/conda create -y -n raspi_venv python=3.6 pip \
     && rm -f ./Miniconda3-latest-Linux-armv7l.sh \
     && echo "export PATH=\"~/bin/conda/bin/:\$PATH\"" >> ${HOME}/.bashrc
