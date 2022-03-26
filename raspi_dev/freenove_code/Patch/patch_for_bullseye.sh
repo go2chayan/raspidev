576566eee9a26d706716e86246bb4a6e448f3919
Md Iftekhar Tanveer <iftekhart@spotify.com>
2022-03-25 23:20:11 -0400

    Patch added to build

--- raspi_dev/freenove_code/Patch/patch_for_bullseye.sh
+++ raspi_dev/freenove_code/Patch/patch_for_bullseye.sh
@@ -1,15 +1,19 @@
 #!/bin/sh
-current_file=$0
-cd "$(dirname "$current_file")"
+#current_file=$0
+#cd "$(dirname "$current_file")"
 
 #sudo cp /boot/config.txt /boot/config-backup.txt
 #sudo cp ./config.txt /boot/config.txt
 
-sudo cp /usr/lib/arm-linux-gnueabihf/libmmal.so /usr/lib/arm-linux-gnueabihf/libmmal-backup.so.backup
-sudo cp ./libmmal.so /usr/lib/arm-linux-gnueabihf/libmmal.so
+issue_date=$(cat /etc/rpi-issue|grep -o "[0-9]*-[0-9]*-[0-9]*")
+threshold_date="2021-10-30"
 
-sudo cp /opt/vc/lib/libmmal.so /opt/vc/lib/libmmal.so.bak 
-sudo cp ./libmmal.so /opt/vc/lib/libmmal.so
-
-echo "patched complete!"
-
+if [[ "$issue_date" > "$threshold_date" ]] ; then
+	sudo cp /usr/lib/arm-linux-gnueabihf/libmmal.so /usr/lib/arm-linux-gnueabihf/libmmal-backup.so.backup
+	sudo cp ./libmmal.so /usr/lib/arm-linux-gnueabihf/libmmal.so
+	sudo cp /opt/vc/lib/libmmal.so /opt/vc/lib/libmmal.so.bak 
+	sudo cp ./libmmal.so /opt/vc/lib/libmmal.so
+	echo "patch complete!"
+else
+	echo "patch not required"
+fi
